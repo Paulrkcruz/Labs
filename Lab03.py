aa7499e6001492053c609fc67d64963dcaeb1226
Paulrkcruz <55728390+Paulrkcruz@users.noreply.github.com>
2019-09-30 18:27:02 -0700

    Add files via upload

---
+++ Lab03.py
@@ -0,0 +1,6 @@
+# Write a program to draw a rectangle on the command line.
+# Prompt the user for a desired width in columns (assume good input).
+# Prompt the user for a desired height in rows (assume good input).
+# Prompt the user for a character that you will use to draw the rectangle. The character
+# can be any single letter, number, or punctuation mark except space (assume good
+# input).
