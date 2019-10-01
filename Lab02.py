13a4b10eec9509b6a8ce28d5b1a5255cd2fc0924
Paulrkcruz <55728390+Paulrkcruz@users.noreply.github.com>
2019-09-30 18:31:59 -0700

    Update Lab02.py

--- Lab02.py
+++ Lab02.py
@@ -13,44 +13,44 @@
 # CORRECT!
 def main():
 
-print("Time for a Quiz! Get prepared. And... NO GOOGLE!")
+  print("Time for a Quiz! Get prepared. And... NO GOOGLE!")
 # Question 1
-    Q1 = input("Where in Seattle is the BEST French Bakery? A. Bakery Nuevo B. Flours C. Kurts")
-    if Q1 == "A":
-        quest1 = "Correct!"
-    else:
-        quest1 = "Incorrect! Visit Bakery Nuevo in Capitol Hill for the best French Deserts!"
+  Q1 = input("Where in Seattle is the BEST French Bakery?   A. Bakery Nuevo   B. Flours   C. Kurts ")
+  if Q1 == "A":
+    quest1 = "Correct!"
+  else:
+    quest1 = "Incorrect! Visit Bakery Nuevo in Capitol Hill for the best French Deserts!"
 # Question 2
-    Q2 = input("What is Seattles Famous landmark? A. UW B. Columbia Tower C. Space Needle")
-    if Q1 == "C":
-        quest2 = "Correct!"
-    else:
-        quest2 = "Incorrect! Google it!"   
+  Q2 = input("What is Seattles Famous landmark?   A. UW   B. Columbia Tower   C. Space Needle ")
+  if Q1 == "C":
+    quest2 = "Correct!"
+  else:
+    quest2 = "Incorrect! Google it!"   
 # Question 3
-    Q3 = input("Where is NEU in California located? A. Los Angeles B. Oakland C. Silicon Valley")
-    if Q1 == "C":
-        quest3 = "Correct!"
-    else:
-        quest3 = "Incorrect! Look it up on NEU.edu!"   
+  Q3 = input("Where is NEU in California located?   A. Los Angeles   B. Oakland   C. Silicon Valley ")
+  if Q1 == "C":
+    quest3 = "Correct!"
+  else:
+    quest3 = "Incorrect! Look it up on NEU.edu!"   
 # Question 4
-    Q4 = input("Where in Seattle is the best neighborhood? A. Capitol Hill B. West Seattle C. Lower Queen Anne")
-    if Q4 == "C":
-        quest4 = "Correct!"
-    else:
-        quest4 = "Incorrect! Lower Queen Anne is the best!"
+  Q4 = input("Where in Seattle is the best neighborhood?   A. Capitol Hill   B. West Seattle   C. Lower Queen Anne ")
+  if Q4 == "C":
+    quest4 = "Correct!"
+  else:
+    quest4 = "Incorrect! Lower Queen Anne is the best!"
 # Question 5
-    Q5 = input("Where can you eat the best pie? A. McDonalds B. At UPS C. Serious Pie in SLU")
-    if Q1 == "C":
-        quest5 = "Correct!"
-    else:
-        quest5 = "Incorrect! Visit Serious Pie in SLU!!"
+  Q5 = input("Where can you eat the best pie?   A. McDonalds   B. At UPS   C. Serious Pie in SLU ")
+  if Q1 == "C":
+    quest5 = "Correct!"
+  else:
+    quest5 = "Incorrect! Visit Serious Pie in SLU!!"
         
-print("Thank you for taking this quiz! Your Results are:")
-print("Question 1:", quest1)
-print("Question 2:", quest2)
-print("Question 3:", quest3)
-print("Question 4:", quest4)
-print("Question 5:", quest5)
-print("Better Luck Next Time!")
+  print("Thank you for taking this quiz! Your Results are:")
+  print("Question 1:", quest1)
+  print("Question 2:", quest2)
+  print("Question 3:", quest3)
+  print("Question 4:", quest4)
+  print("Question 5:", quest5)
+  print("Better Luck Next Time!")
 
 main()
